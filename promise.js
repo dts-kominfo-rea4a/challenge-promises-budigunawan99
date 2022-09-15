7fa8233c9061bd2514797663c7720cda149f8f54
Budi Gunawan <gunawanbudi930@gmail.com>
2022-09-16 01:17:36 +0700

    Add promiseOutput function
    function to count object property value as assigned in function parameter

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasil) => {
+  const data = [...(await promiseTheaterIXX()), ...(await promiseTheaterVGC())];
+  return data.reduce((total, item) => {
+    if (item.hasil === hasil) {
+      return total + 1;
+    }
+    return total + 0;
+  }, 0);
+};
 
 module.exports = {
   promiseOutput,
